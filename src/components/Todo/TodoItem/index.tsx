b93851c0b29c045b7e923d5dd0144205620048ce
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-18 09:15:09 +0530

    add file index.tsx in Todo

--- src/components/Todo/TodoItem/index.tsx
+++ src/components/Todo/TodoItem/index.tsx
@@ -1,2 +1,8 @@
 
 import React from 'react'
+
+const TodoItem: React.FC = () => {
+    return <li>Code</li>;
+};
+
+export default TodoItem;
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Todo: React.FC = () => {
+    return (
+        <div>
+            <ol>
+                
+            </ol>
+        </div>
+    )
+}
